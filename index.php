a781e8fecdc973a6c065b47e01e9e8cda891802a
ozgurkokturk <ozgurkokturk0@gmail.com>
2020-02-20 22:13:10 +0200

    dosya upload islemleri

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>Form Uygulamaları</h2>
+<h3>Dosya upload işlemi </h3>
+<hr />
+<form action="yukle.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="dosya" />
+    <input type="submit" name="gonder" value="Yükle" />
+</form>
+
+</body>
+</html>
