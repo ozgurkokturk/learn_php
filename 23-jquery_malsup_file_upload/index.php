8d3cc48a16071a1ab94ddd4f66e3199eec95cbf8
ozgurK <ozgurkokturk0@gmail.com>
2020-06-10 15:50:43 +0300

    progress bar eklendi

--- 23-jquery_malsup_file_upload/index.php
+++ 23-jquery_malsup_file_upload/index.php
@@ -5,62 +5,57 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     <script>
-        // wait for the DOM to be loaded
         $(document).ready(function() {
 
-            function validate(formData, jqForm, options) {
-                var durum=1;
+            $("#myForm").submit(function (event) {
 
-                jqForm.find('.zorunlu').each(function(){
-                    var eleman=$(this);
-                    var deger=eleman.val();
+                var options = {
+                    url: 'upload.php',
+                    method: 'POST',
+                    target: $(".targetLayer"),
+                    beforeSubmit: function () {
+                        $(".progress").show();
+                        $(".progress-bar").width('%0');
+                    },
+                    uploadProgress: function(event,position,total,percentComplete){
+                        $(".progress-bar").width(percentComplete + '%');
+                        $(".progress-bar").html(percentComplete + '%');
+                    },
+                    complete: function(cevap) {
+                        $(".targetLayer").show();
+                    },
+                    resetForm: true
+                };
 
-                    if (deger=="" || deger=="0" || deger==null)
-                    {
-                        durum=0;
-                    }
-                })
-
-                //elemanları pasif ediyoruz
-                if(durum==1) { alert('elemanlar tamam') }
-                else
-                {
-                    alert('Lütfen doldurulması gereken alanları boş bırakmayınız.');
-                    return false;
+                // bu kontrolü sağlamazsak dosya seçmeden butona basınca da progress çalışıyo :S
+                if($(".dosya").val()){
+                    event.preventDefault();
+                    $("#myForm").ajaxSubmit(options);
                 }
-            }
 
-            var options = {
-                url: 'upload.php',
-                dataType: 'json',
-                method: 'POST',
-                beforeSerialize: function($form, options) {
-                    alert('başlıyor');
-                },
-                beforeSubmit: validate,
-                success:    function() {
-                    alert('Tamam!');
-                }
-            };
-
-            jQuery('.ajaxform').ajaxForm(options);//json
+                return false;
+            });
         });
     </script>
 </head>
+
 <body>
 <div class="container mt-4">
     <div class="row">
         <div class="col-md-6">
+
             <form id="myForm" method="post" enctype="multipart/form-data">
-                <input type="file" name="dosya" class="zorunlu"> <br> <br>
-                <div class="progress" style="width: 30vh">
-                    <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 40%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
-                        %40
+                <input type="file" name="dosya" class="dosya"> <br> <br>
+                <div class="progress" style="width: 30vh; display: none;">
+                    <div class="progress-bar progress-bar-striped" role="progressbar">
+
                     </div>
                 </div>
                 <br> <br>
                 <input type="submit" value="Yükle" />
             </form>
+            <div class="targetLayer" style="display: none;"></div>
+
         </div>
     </div>
 </div>
